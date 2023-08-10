19b5ab260569a88153788c3c33975e1894b1bb06
remorses <beats.by.morse@gmail.com>
2023-08-10 11:40:54 +0200

    nn

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Reproduction
 -   click the button `navigate to another/random`
 -   Notice that the param `slug` is not an array of paths, but it's url encoded (something like `["another%2Frandom"]`)
 
-Example 
+Example deployment: 
--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -1,15 +1,24 @@
 import Image from 'next/image'
-import styles from '../page.module.css'
+
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 export default function Home({ params }: any) {
     const random = Math.random().toString(36).substring(7)
+    const router = useRouter()
     return (
-        <main className={styles.main}>
+        <main>
             {JSON.stringify({ params })}
-            <Link href={`/another/{random}`}>
-                Navigate to /another/{random}
+            <Link href={`/another/${random}`}>
+                Navigate with link to /another/{random}
             </Link>
+            <button
+                onClick={() => {
+                    router.push(`/another/${random}`)
+                }}
+            >
+                Navigate with router /another/{random}
+            </button>
         </main>
     )
 }

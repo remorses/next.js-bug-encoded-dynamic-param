7a3b37483fd64c7e39c357c3d7d01deec5ba81a9
remorses <beats.by.morse@gmail.com>
2023-08-10 11:36:39 +0200

    app/[[...slug]]/page:

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -1,6 +1,15 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home({ params }: any) {
-    return <main className={styles.main}>{JSON.stringify({ params })}</main>
+    const random = Math.random().toString(36).substring(7)
+    return (
+        <main className={styles.main}>
+            {JSON.stringify({ params })}
+            <Link href={`/another/{random}`}>
+                Navigate to /another/{random}
+            </Link>
+        </main>
+    )
 }
